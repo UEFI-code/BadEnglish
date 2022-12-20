3d2552b445bfcf205209c631b9ef3607a3be0863
SuperHacker UEFI <1092700291@qq.com>
2022-12-20 15:51:23 +0800

    Update ex.py

--- SeqGAN-PyTorch/ex.py
+++ SeqGAN-PyTorch/ex.py
@@ -1,7 +1,7 @@
 import numpy as np
 from coco_loader import coco_loader
 from torch.utils.data import DataLoader
-train_data = coco_loader('/mnt/data1/wch/data/cucu/coco/coco2014', split='train', ncap_per_img=5)
+train_data = coco_loader('/coco2014', split='train', ncap_per_img=5)
 train_data_loader = DataLoader(dataset=train_data, num_workers=16,batch_size=1)
 for batch_idx, (imgs, captions, wordclass, mask, _) in enumerate(train_data_loader):
    ls = wordclass.numpy().tolist()
